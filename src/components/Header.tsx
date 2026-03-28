964f125bfd4c4e66d95f314d8d9a4e75e0856472
A1exxander <raposoalexander@gmail.com>
2026-03-27 22:24:32 -0400

    mend

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,11 @@ export default function Header({ renderHomePageBtn = false }: HeaderProps): Reac
         <div className="flex px-2 py-4 items-center">
 
             <div className="w-28"> {/* Preallocate space to prevent layout shift */}
-            {renderHomePageBtn && (
-                <Link to="/" className="text-gold p-1 px-2 border-2 border-gold bg-transparent font-semibold text-sm rounded transition-colors duration-300 hover:bg-darkgrey hover:text-gold">
-                    <span>Return Home</span>
-                </Link>
-            )}
+                {renderHomePageBtn && (
+                    <Link to="/" className="text-gold p-1 px-2 border-2 border-gold bg-transparent font-semibold text-sm rounded transition-colors duration-300 hover:bg-darkgrey hover:text-gold">
+                        <span>Return Home</span>
+                    </Link>
+                )}
             </div>
 
             <h1 className="flex-1 text-center text-lg text-gold font-semibold">
