570187c93cd0a1e1e579bd60fdcefc9e34c05d3c
A1exxander <raposoalexander@gmail.com>
2026-03-27 20:19:59 -0400

    Configured default starting colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ export default {
       md: { max: "767px" },
       sm: { max: "639px" },
     },
+    colors: {
+      darkgreen: '#0d4a2f',     // the green — Mainly just the table
+      gold:{                      // Used for most things - Outlines, buttons, highlights, text etc
+        DEFAULT: "#d4af37",
+        light: "#f5d86e",
+      }  ,
+      darkgrey: '#2e2d2d',      // dark grey — chips, overlays
+      offwhite:   '#ede6d2',    // off-white — body text
+    },
     extend: {},
   },
   plugins: [],
