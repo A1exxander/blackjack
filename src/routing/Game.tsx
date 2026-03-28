a053544b6bdc7d7278592cabdab105a047bca90c
A1exxander <raposoalexander@gmail.com>
2026-03-27 22:29:53 -0400

    Added game page

---
+++ src/routing/Game.tsx
@@ -0,0 +1,11 @@
+import { Body } from "./Body"
+
+export default function Game(): React.ReactElement {
+    return (
+        <Body>
+            <main>
+                <h2 className="sr-only">Game Page</h2>
+            </main>
+        </Body>
+    );
+}
