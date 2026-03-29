736d06bf1615e4b8384038067a35c2b7caef50f1
A1exxander <raposoalexander@gmail.com>
2026-03-29 16:03:30 -0400

    Removed StyledLink as it was too inflexibe

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import StyledLink from "./StyledLink";
+import { Link } from "react-router-dom";
 import UnderlineEffect from "./UnderlineEffect";
 
 interface HeaderProps {
@@ -11,9 +11,9 @@ export default function Header({ renderHomePageBtn = false }: HeaderProps): Reac
             <div className="flex items-center gap-2 p-4 justify-between">
                 <div className="flex justify-start flex-shrink-0 order-last sm:order-first w-24 sm:w-36"> {/* Preallocate space to prevent layout shift when button appears */}
                     {renderHomePageBtn && (
-                        <StyledLink variant="gold-button" route = "/">
+                        <Link className = "text-gold p-1 px-2 border-2 border-gold bg-transparent font-semibold text-md rounded transition-colors duration-300 hover:bg-darkgrey hover:text-gold" to = "/">
                             <span className="before:content-['Home'] sm:before:content-['Return_Home'] text-center text-sm md:text-md" />
-                        </StyledLink>
+                        </Link>
                     )}
                 </div>
                 <h1 className="font-display text-lg order-first sm:order-none font-display flex-1 text-center text-xl lg:text-2xl text-gold font-semibold tracking-wide">
--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import StyledLink from "../components/StyledLink";
 import { Body } from "./Body";
 import { generateUUID } from "../utils/UUIDUtils";
 import { Link } from "react-router-dom";
@@ -14,16 +13,17 @@ export default function HomePage(): React.ReactElement {
         <Body>
             <main className = "h-full flex flex-col items-center justify-center">
                 <h2 className = "sr-only">Home Page</h2>
-                <section className="pt-2 overflow-hidden flex flex-[6] flex-col space-around justify-around items-center">
-                    <h3 className="text-xl text-offwhite">WELCOME {username}</h3>
-                    <StyledLink variant="gold-button" route = {`/game/${gameID}`}>
-                        <span className="text-xl">Start Game</span>
-                    </StyledLink>
+                <section className="w-full overflow-hidden flex flex-[6] flex-col justify-around items-center">
+                    <h3 className="text-xl text-offwhite">Welcome {username}</h3>
+                    <div></div>
+                    <Link className="text-gold p-2 px-32 border-2 border-gold bg-transparent font-semibold text-md rounded transition-colors duration-300 hover:bg-darkgrey hover:text-gold" to={`/game/${gameID}`}>
+                        <span>Start Game</span>
+                    </Link>
                 </section>
               <footer className="w-full flex-1 flex flex-col items-center">
-                <div className="absolute left-1/2 w-full h-1 -translate-x-1/2 bg-gradient-to-r from-transparent via-offwhite to-transparent"></div>
+                <div className="absolute left-1/2 w-full h-1 -translate-x-1/2 bg-gradient-to-r from-transparent via-gold to-transparent"></div>
                 <Link className="pt-8 w-fit text-center" to="/settings">
-                    <span className="text-offwhite text-xl transition-colors duration-300 hover:text-gold">Settings</span>
+                    <span className="text-offwhite text-md transition-colors duration-300 hover:text-gold">Settings</span>
                 </Link>
             </footer>
             </main>
