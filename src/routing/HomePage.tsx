1625a2093604de9506af2eb1d63790432da007c8
A1exxander <raposoalexander@gmail.com>
2026-03-29 14:47:01 -0400

    Added homepage WIP design

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import StyledLink from "../components/StyledLink";
 import { Body } from "./Body";
+import { generateUUID } from "../utils/UUIDUtils";
 
 export default function HomePage(): React.ReactElement {
+
+    const [gameID] = useState<String>((() => generateUUID()));
+    
     return (
         <Body>
-            <main>
+            <main className = "h-full flex flex-col items-center justify-center">
                 <h2 className = "sr-only">Home Page</h2>
+                <section className="bg-black w-full flex flex-[4] flex-col space-around justify-around items-center">
+                    <h3 className="text-xl text-offwhite">WELCOME Alexander</h3>
+                    <StyledLink variant="gold-button" route = {`/game/${gameID}`}>
+                        <span className="text-xl">Start Game</span>
+                    </StyledLink>
+                </section>
+                <footer className = "w-full flex-1">
+                </footer>
             </main>
         </Body>
     );
